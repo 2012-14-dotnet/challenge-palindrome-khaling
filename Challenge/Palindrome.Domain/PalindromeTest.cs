0403f5ad95aaae5e8305dec26d208ebef889334c
Nar <rohini.smi2010@gmail.com>
2021-01-04 12:37:11 -0600

    Add challenge

---
+++ Challenge/Palindrome.Domain/PalindromeTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Palindrome.Domain
+{
+    public class PalindromeTest
+    {
+        public bool CheckPalindrome(string str)
+        {
+            var name= Regex.Replace(str, @"[^0-9a-zA-Z\._]", "");
+            string first = name.ToLower().Substring(0, name.Length / 2);
+            char[] arr   = name.ToCharArray();
+
+            Array.Reverse(arr);
+
+            string temp   = new string(arr);
+            string second = temp.Substring(0, temp.Length / 2);
+
+            return first.Equals(second);
+        }
+    }
+
+
+}
